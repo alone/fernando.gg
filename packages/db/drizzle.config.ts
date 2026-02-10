9e5f8477da99908a76d4e1aeda413f1aae9e5eae
f2 <modernbaseball@tuta.com>
2026-02-09 16:29:45 -0800

    fix: added a workaround to be able to run db commands from root path

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,14 @@
+import { resolve } from "node:path";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: resolve(__dirname, "../../apps/server/.env") });
 
 export default defineConfig({
 	schema: "./src/schema",
 	out: "./src/migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: Bun.env.DATABASE_URL ?? "",
+		url: process.env.DATABASE_URL ?? "",
 	},
 });
